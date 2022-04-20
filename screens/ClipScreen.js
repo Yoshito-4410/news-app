e88352975e6a8499705a9c4915f2f649838590c7
Yoshito-4410 <ttymah@icloud.com>
2022-04-21 07:36:26 +0900

    add : クリップした記事を表示

--- screens/ClipScreen.js
+++ screens/ClipScreen.js
@@ -1,4 +1,6 @@
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView, Text, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import ListItem from "../components/ListItem";
 
 const styles = StyleSheet.create({
   container: {
@@ -7,10 +9,23 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ClipScreen = () => {
+export default ClipScreen = ({ navigation }) => {
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ClipScreen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            ImageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate("Article", { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 };
