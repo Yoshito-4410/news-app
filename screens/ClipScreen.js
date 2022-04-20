7c1314c18791f10239cd380a5a8bf104ee7af9d2
Yoshito-4410 <ttymah@icloud.com>
2022-04-20 15:38:45 +0900

    add : ボトムタブバーを作成

---
+++ screens/ClipScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, SafeAreaView, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
+
+export default ClipScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>ClipScreen</Text>
+    </SafeAreaView>
+  );
+};
